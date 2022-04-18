b250dba95b6a2fdd7841f96ca8b6efc4017e084f
Nikolas <timofeevnik41@gmail.com>
2022-04-18 13:09:19 +0300

    :zap: Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
-<script>
-import Login from "./components/Login.vue";
-
-export default {
-  components: { Login },
-};
-</script>
+<script></script>
 
 <template>
   <router-view></router-view>
--- src/routing.js
+++ src/routing.js
@@ -1,7 +1,11 @@
 import LoginPage from "@/views/LoginPage.vue";
+import SchedulePage from "@/views/SchedulePage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [{ path: "/login", component: LoginPage }];
+const routes = [
+  { path: "/login", component: LoginPage },
+  { path: "/schedule", component: SchedulePage },
+];
 
 const router = createRouter({
   history: createWebHistory(),
