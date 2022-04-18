1ca6306b8aa0e5a1cb627938f6ad352dc980c9e5
Nikolas <timofeevnik41@gmail.com>
2022-04-18 13:55:05 +0300

    :zap: Schedule page + components improved

--- src/views/SchedulePage.vue
+++ src/views/SchedulePage.vue
@@ -1,10 +1,31 @@
 <template>
-  <user-info />
-  <schedule />
+  <div class="container">
+    <div class="row">
+      <user-info />
+    </div>
+    <div class="row">
+      <p class="h-custom">Запись</p>
+    </div>
+    <div class="row">
+      <p class="sub-h">
+        Статус: <a class="inactive">не записан</a> или
+        <a class="active">запись на {{ scheduledTime }}</a>
+      </p>
+    </div>
+    <div class="row">
+      <schedule />
+    </div>
+    <div class="row">
+      <p class="sub-h inactive">
+        Нажмите на свободное место, чтобы записаться на соотвесутюущю установку
+        и время.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import Schedule from "../components/Schedule.vue";
+import Schedule from "../components/Schedule/Schedule.vue";
 import UserInfo from "../components/UserInfo.vue";
 
 export default {
@@ -12,7 +33,29 @@ export default {
     UserInfo,
     Schedule,
   },
+  data() {
+    return {
+      scheduledTime: "12:00",
+    };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.h-custom {
+  color: var(--main-font-color);
+  font-size: 24px;
+}
+.sub-h {
+  color: var(--main-font-color);
+  font-size: 16px;
+}
+.inactive {
+  color: var(--primary-color-dark-gray);
+  text-decoration: none;
+}
+.active {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
