1c3bddae02f45b50df0e479b51d7321e57106764
Nikolas <timofeevnik41@gmail.com>
2022-04-18 13:09:03 +0300

    :zap: user-info component started

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- Дописать компонент -->
+  <p>Информация о пользователе</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      default() {
+        return "User";
+      },
+    },
+    isu_number: {
+      type: String,
+      default() {
+        return "000000";
+      },
+    },
+    avatar_link: {
+      type: [String, null],
+      default() {
+        return null;
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
