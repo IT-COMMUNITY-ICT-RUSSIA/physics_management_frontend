70c1d906a40ca6b38c6a8a9881be666043388fee
Nikolas <timofeevnik41@gmail.com>
2022-04-19 16:32:40 +0300

    :zap: Icons

---
+++ src/components/Icons/SignInIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <img
+    src="https://img.icons8.com/ios/100/000000/login-rounded-right--v1.png"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Icons/TestUserIcon.vue
@@ -0,0 +1,9 @@
+<template>
+  <img src="https://img.icons8.com/ios/100/000000/test-account.png" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
