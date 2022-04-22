a70d8d7fee2f9d4e0ff5d8d019a399d633c47acf
Nikolas <timofeevnik41@gmail.com>
2022-04-22 14:36:33 +0300

    :zap: Empty slot icon implemented

---
+++ src/components/Icons/EmptySlotIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="empty-slot"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.empty-slot {
+  width: 80px;
+  height: 80px;
+
+  background: #ffffff;
+  border: 3px dashed #c4c4c4;
+  border-radius: 80px;
+  box-sizing: border-box;
+}
+</style>
