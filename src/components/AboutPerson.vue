dc7781df4eceab8108c6059571711caf566560dd
zemljanichka <belkazarina@mail.ru>
2022-06-07 22:50:56 +0300

    Upd AboutPage

---
+++ src/components/AboutPerson.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card text-center" id="card">
+        <img :src="personAvatar" id="img">
+        <div class="card-body">
+          <h5 class="card-title">{{ personName }}</h5>
+          <p class="card-text" style="height: 100px">{{ personInfo }}</p>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    props: {
+        personName: "",
+        personInfo: "",
+        personImg: "",
+    },
+    computed: {
+        personAvatar() {
+        return this.personImg
+            ? this.personImg
+            : `https://st.depositphotos.com/2024219/3404/v/950/depositphotos_34046177-stock-illustration-silhouette-of-business-man-with.jpg`;
+        },
+    }
+};
+</script>
+
+<style>
+#card {
+    margin-bottom: 20px;
+    border: 3px solid;
+    border-color: #2e4ba5;
+}
+
+#img {
+    margin: 5px;
+}
+</style>
--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -6,37 +6,58 @@
     <div class="row justify-content-center">
       <div class="col-md-auto align-self-center">
         <p class="h-custom text-center">
-          Кто мы такие и что вообще тут забыли
+          Кто мы такие.
           <br />
-          Новая строка
-          <br />
+          Что тут забыли
           <a id="change-text">
             {{ textItems[textIndex] }}
           </a>
           . Продолжение текста
           <br />
-          Еще что-то про нас
-          {{ textIndex }}
+          <br />
+          Команда
         </p>
       </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-3">
+      <span v-for="i in [0, 1, 2, 3, 4, 5, 6, 7, 8]" :key="i"> 
+        <div class="col">
+          <about-person 
+          :personName="personNames[i]"
+          :personInfo="personRoles[i]"
+          />
+        </div>  
+      </span>
     </div>
   </div>
 </template>
 
 <script>
+import AboutPerson from "../components/AboutPerson.vue";
+
 export default {
+  components: { AboutPerson },
   methods: {
     countTextIndex() {
         setTimeout(() => {
           this.textIndex = (this.textIndex+1)%3;
           this.countTextIndex();
-        }, 2000);
+        }, 3000);
     },
   },
   data() {
     return {
       textIndex: 0,
       textItems: ["Вариант 1", "Вариант 2", "Вариант 3"],
+      personNames: ["Тимофеев Николай", "Береза Никита", "Барышева Зарина", 
+                    "Кобелев Лев", "Кормановская Дарина", "Савкин Владислав",
+                    "Еремеева Арина", "Паргачевский Николай", "Борсов Никита"],
+      personRoles: ["Что сделал человек 1", "Что сделал человек 2", "Что сделал человек 3",
+                    "Что сделал человек 4", "Что сделал человек 5", "Что сделал человек 6",
+                    "Что сделал человек 7", "Что сделал человек 8", "Что сделал человек 9"],
+      /*personImages: [""],*/
     };
   },
   created: function () {
